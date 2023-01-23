8aa9feb6bc68382e0d150c89e897a91a3b2bacf5
Antoine <65955606+Antoine-Flo@users.noreply.github.com>
2023-01-23 17:19:37 +0100

    clean

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// This is a list of orders from a restaurant
+
 const orders = [
   { id: 12, customer: 'Jane Doe', date: '01/01/2022', total: 75 },
   { id: 1, customer: 'John Smith', date: '01/01/2022', total: 50 },
@@ -13,31 +15,30 @@ const orders = [
   { id: 9, customer: 'John Smith', date: '01/12/2022', total: 30 },
 ]
 
-
-// Create a function called "filterByDateRange" that takes in the array of orders, a start date, and an end date as arguments, and returns a new array containing only the orders placed within that date range.
-
-// Create a function called "groupByCustomer" that takes in the array of orders and returns an object where the keys are customer names and the values are arrays of orders placed by that customer.
-// exemple {
-//   "John Smith": [
-//       { id: 1, customer: "John Smith", date: "01/01/2022", total: 50 },
-//       { id: 3, customer: "John Smith", date: "01/05/2022", total: 25 },
-//       { id: 5, customer: "John Smith", date: "01/09/2022", total: 30 },
-//         ...
-//   ],
-//   "Jane Doe": [
-//       { id: 2, customer: "Jane Doe", date: "01/03/2022", total: 75 },
-//       { id: 4, customer: "Jane Doe", date: "01/07/2022", total: 100 },
-//         ...
-//   ],
-//   ...
-// }
-
-// Create a function called "averageOrder" that takes in the array of orders and returns the average order total.
-
-
-// At the end you should be able to create a function averageOrderByDateByCustomer that utalize the previous ones
-// that takes a cutomer name, a date range in an object, and return the avergae order for that customer in that date range
+// At the end of the exercice you should be able to create a function averageOrderByDateByCustomer
+// It takes a cutomer name, a date range in an object, and return an object with the avergae order for that customer in that date range
 const result = averageOrderByDateByCustomer('Jane Doe', {
   start: '01/02/2022',
   end: '01/24/2022',
-}) // 68
+})
+console.log(result)
+// {
+//  customer: 'Jane Doe',
+//  period: {
+//   start: '01/02/2022',
+//   end: '01/24/2022',
+//  },
+//  averageOrder: 68
+//}
+
+// For that you will create several small functions that can be used independently
+// Here is a possible way to organize your code, you can use other names or organize it differently
+// as long as you create at least three different functions, you can create the function you want
+
+// Create a function called "groupByCustomer" that takes in an array of orders
+// and returns an array of order for that customer groupByCustomer(orders, 'Jane Doe')
+
+// Create a function called "filterByDateRange" that takes in an array of orders,
+// a start date, and an end date as arguments, and returns a new array containing only the orders placed within that date range
+
+// Create a function called "averageOrder" that takes in an array of orders and returns the average order total.
